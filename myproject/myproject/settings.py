9306571c51a0014357502f5ad2a746175b51a946
Milesi <you@example.com>
2023-09-05 19:31:42 +0300

    HW6

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q93!0e5j#(l(*%^hjv4+n-njs-0r8pd*@he^_2%-pid364y0!e'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['milesi.pythonanywhere.com',]
 
 
 # Application definition
@@ -77,8 +79,15 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'milesi$default',
+        'USER': 'milesi',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'milesi.mysql.pythonanywhere-services.com',
+        'OPTIONS':{
+            'init_command': "SET NAMES 'utf8md4'; SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8md4',
+        },
     }
 }
 
@@ -118,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
